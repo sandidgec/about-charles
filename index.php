1e843a41babc255cc2e256c8404fd9267e97ae22
TrevorRigler <tarigler@gmail.com>
2015-07-13 16:35:39 -0600

    Trevor completely wrecked Charles' website.

--- index.php
+++ index.php
@@ -5,16 +5,18 @@
 		<title>About Charles</title>
 	</head>
 	<h1> <b> About Charles </b> </h1>
-	<body> <p> I am 34 years old, I was born in South Dakota and my parents moved to Arizona when I was 3 years old.
-		At 18 I joined the Navy and spent 6 years at various bases around the world. I decided to leave the military and
-		join the professional world with a large defense contractor. I disliked the job and decided to make a move to New
+	<body> <p> Charles is 34 years old. He was born in South Dakota and moved with his family to Arizona when he was 3 years old.
+		At 18, Charles joined the Navy and spent 6 years at various bases around the world. Charles decided to leave the military and
+		join the professional world with a large defense contractor. Charles disliked the job and decided to make a move to New
 		Mexico and go to work as a consultant for a non-profit that focused on economic development. </p>
 
-		<p> I decided in order to help my professional development I would take the Deep Dive Coding course in order
-		to add to my skill set and help become more marketable to employers.</p>
+		<p> Charles decided in order to help my professional development he would take the Deep Dive Coding course in order
+		to add to his skill set and help become more marketable to employers.</p>
 
-		<p> I also own a year old english bulldog named Holly</p>
-		<img src="C:\Users\sandidgec\Pictures\2014_06_15\IMG_0369.jpg" alt="Puppy Pic" style="width:128px;height:128px;">
+		<p> Charles also owns a year old english bulldog named Holly</p>
+		<img src="randombulldog.jpg" alt="NOT HOLLY" style="width:128px;height:128px;">
+		<p> This is not Holly. </p>
+		<p> Charles recently competed in the World Series of Poker.</p>
 	</body>
 
 </html>
