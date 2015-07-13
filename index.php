01d471fac00f965752cd455b384b68930bce6ec9
Charles Sandidge <sandidgec@gmail.com>
2015-07-13 15:48:33 -0600

    Adding basic html to describe myself (Charles).

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8">
 		<title>About Charles</title>
 	</head>
-	<h1> About Charles</h1>
+	<h1> <b> About Charles </b> </h1>
 	<body> <p> I am 34 years old, I was born in South Dakota and my parents moved to Arizona when I was 3 years old.
 		At 18 I joined the Navy and spent 6 years at various bases around the world. I decided to leave the military and
 		join the professional world with a large defense contractor. I disliked the job and decided to make a move to New
