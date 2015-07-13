bb53a19855775b6f04757abcbf277a61ccc12603
Charles Sandidge <sandidgec@gmail.com>
2015-07-13 15:34:47 -0600

    Adding basic html to describe myself (Charles).

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 		join the professional world with a large defense contractor. I disliked the job and decided to make a move to New
 		Mexico and go to work as a consultant for a non-profit that focused on economic development. </p>
 
-		<p> I then decided in order to help my professional development I would take the Deep Dive Coding course in order
+		<p> I decided in order to help my professional development I would take the Deep Dive Coding course in order
 		to add to my skill set and help become more marketable to employers. </p>
+		<img src="C:\Users\sandidgec\Pictures\2014_06_15\IMG_0369.jpg" alt="Puppy Pic" style="width:128px;height:128px;">
 	</body>
 
-	<img src="C:\Users\sandidgec\Pictures\2014_06_15\IMG_0369.jpg" alt="Puppy Pic" style="width:128px;height:128px;">
 </html>
