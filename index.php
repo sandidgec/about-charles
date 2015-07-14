84de1bff0bfa85a912c50ecfdf374de53f553da0
TrevorRigler <tarigler@gmail.com>
2015-07-14 09:01:46 -0600

    additional text

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 	<h1> <b> About Charles </b> </h1>
 	<body> <p> Charles,a US Navy veteran, has lived in Arizona, South Dakota, and various naval bases around the world.</p>
 		<p> Charles can see an entirely different color spectrum than most human beings. He can also communicate with shellfish.</p>
-		<p> Charles also owns a year old english bulldog named Holly</p>
+		<p> Charles also owns a year old english bulldog named Holly...</p>
 		<img src="randombulldog.jpg" alt="NOT HOLLY" height="397" width="522">
-		<p>This is not Holly. </p>
+		<p>...but this is <b>NOT</b> Holly. </p>
 		<p> Charles recently competed in the World Series of Poker. "Charles" is an alias assigned by the federal Witness Protection Program. "Charles" has provided key testimony against organized crime cartels in the gambling industry.</p>
 	</body>
 
