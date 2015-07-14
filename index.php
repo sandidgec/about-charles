8883ced9a258b7752253333b287d2d93403d7750
TrevorRigler <tarigler@gmail.com>
2015-07-14 08:58:18 -0600

    additional text

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		<p> Charles can see an entirely different color spectrum than most human beings. He can also communicate with shellfish.</p>
 		<p> Charles also owns a year old english bulldog named Holly</p>
 		<img src="randombulldog.jpg" alt="NOT HOLLY" style="width:128px;height:128px;">
-		<p> This is not Holly. </p>
+		<p>This is not Holly. </p>
 		<p> Charles recently competed in the World Series of Poker. "Charles" is an alias assigned by the federal Witness Protection Program. "Charles" has provided key testimony against organized crime cartels in the gambling industry.</p>
 	</body>
 
