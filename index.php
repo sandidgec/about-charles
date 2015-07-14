6f2408492c8846818cf8e6c8a96b1e178dc856d8
TrevorRigler <tarigler@gmail.com>
2015-07-14 08:57:13 -0600

    additional text

--- index.php
+++ index.php
@@ -5,14 +5,8 @@
 		<title>About Charles</title>
 	</head>
 	<h1> <b> About Charles </b> </h1>
-	<body> <p> Charles is 34 years old. He was born in South Dakota and moved with his family to Arizona when he was 3 years old.
-		At 18, Charles joined the Navy and spent 6 years at various bases around the world. Charles decided to leave the military and
-		join the professional world with a large defense contractor. Charles disliked the job and decided to make a move to New
-		Mexico and go to work as a consultant for a non-profit that focused on economic development. </p>
-
-		<p> Charles decided in order to help his professional development he would take the Deep Dive Coding course in order
-		to add to his skill set and help become more marketable to employers.</p>
-
+	<body> <p> Charles,a US Navy veteran, has lived in Arizona, South Dakota, and various naval bases around the world.</p>
+		<p> Charles can see an entirely different color spectrum than most human beings. He can also communicate with shellfish.</p>
 		<p> Charles also owns a year old english bulldog named Holly</p>
 		<img src="randombulldog.jpg" alt="NOT HOLLY" style="width:128px;height:128px;">
 		<p> This is not Holly. </p>
